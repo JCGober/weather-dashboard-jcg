6a09971b71c0125c3add279294085148ce391ce8
Jared C. Gober <jcgober@Jareds-MacBook-Air.local>
2020-08-14 18:11:34 -0400

    Got append forecasts working

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,10 @@ var cityButtonArea = $("#cityBtn")
 
 var displayCurrent = $("#displayCurrent")
 
-var city ="Charlotte"
+var displayForecast = $("#displayForecast")
 
+var city = "Charlotte"
+ 
 var queryURL="https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=510c18f533b70331cb4fe3a2dca04f26"
 
 
@@ -27,8 +29,9 @@ $.ajax({
     console.log(response)
 
     searchBtn.on("click", function(){
+        displayForecast.empty()
 
-        city = input.val()
+        var city = input.val()
 
         queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=510c18f533b70331cb4fe3a2dca04f26"
 
@@ -51,19 +54,22 @@ $.ajax({
         console.log("Humidity: " + response.list[0].main.humidity)
         console.log("Wind Speed: " + response.list[0].wind.speed)
 
-
+        // for loop to run through and append forecasts 
         for(var i = 1; i<5; i++){
+
+            var forcast = ["blank", "Tomorrow:", "Next Day:", "Next Day:", "Next Day:"]
     
             var tempF = Math.round(((response.list[i].main.temp) - 273.15)*(1.8)+32)
     
             var humidity = response.list[i].main.humidity
     
             var windSpeed = response.list[i].wind.speed
-    
-            console.log(city)
-            console.log("Temperature: " + tempF)
-            console.log("humidity: " + humidity)
-            console.log("Wind Speed: " + windSpeed + " MPH")
+
+            var forecastDiv = $("<div>")
+
+            forecastDiv.html("<h1>" +forcast[i] + "</h1><h3>Temperature: " + tempF+ "</h3><h3>humidity: " + humidity + "</h3><h3>Wind Speed: " + windSpeed + " MPH </h3>")
+
+            displayForecast.append(forecastDiv)
         }
     })
 
