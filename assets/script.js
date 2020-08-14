17aae3e9c6689078b7a355e24294a05cd86969b1
Jared C. Gober <jcgober@Jareds-MacBook-Air.local>
2020-08-14 18:15:59 -0400

    Updated CSS

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,7 @@ $.ajax({
     searchBtn.on("click", function(){
         displayForecast.empty()
 
-        var city = input.val()
+        city = input.val()
 
         queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=510c18f533b70331cb4fe3a2dca04f26"
 
