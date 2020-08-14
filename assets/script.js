851e609bfc29cf5744fb23b66c19592c3be46d6c
Jared C. Gober <jcgober@Jareds-MacBook-Air.local>
2020-08-14 17:14:37 -0400

    Added city button to HTML on search click

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var searchBtn = $("#searchBtn")
 
 var input = $("#input")
 
+var cityButtonArea = $("#cityBtn")
+
 var city ="Charlotte"
 
 var queryURL="https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=510c18f533b70331cb4fe3a2dca04f26"
@@ -15,22 +17,43 @@ var queryURL="https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&ap
 
 // Ajax code 
 
-
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
 
     console.log(response)
-    
-    for(var i = 0; i<5; i++){
-        var tempF = Math.round(((response.list[i].main.temp) - 273.15)*(1.8)+32)
-        tempF
-    
-        console.log(tempF)
-    }
 
-})
+    searchBtn.on("click", function(){
 
+        city = input.val()
+
+        queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=510c18f533b70331cb4fe3a2dca04f26"
+
+        var newButton= $("<button>")
+
+        newButton.html(city)
+
+        cityButtonArea.append(newButton)
+
+
+        for(var i = 1; i<5; i++){
+    
+            var tempF = Math.round(((response.list[i].main.temp) - 273.15)*(1.8)+32)
+    
+            var humidity = response.list[i].main.humidity
+    
+            var windSpeed = response.list[i].wind.speed
+
+
+    
+            console.log(city)
+            console.log("Temperature: " + tempF)
+            console.log("humidity: " + humidity)
+            console.log("Wind Speed: " + windSpeed + " MPH")
+        }
+    })
+
+}) 
 
 
